afb75dd962a67c9607fff867331e90bac02fe98b
myunghyun_yang <shiningy84@gmail.com>
2021-03-14 03:30:27 +0900

    10.10 Edit Restaurant Testing

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -26,7 +26,7 @@ export class Restaurant extends CoreEntity {
   @IsString()
   coverImg: string;
 
-  @Field((type) => String, { defaultValue: '강남' })
+  @Field((type) => String)
   @Column()
   @IsString()
   address: string;
