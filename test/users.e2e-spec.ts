7e96b4e1cf4db2ea564924fb244731bd37df0f71
myunghyun_yang <shiningy84@gmail.com>
2021-03-13 15:25:50 +0900

    9.2 Testing createAccount part 1

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -2,22 +2,57 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
+import { getConnection } from 'typeorm';
+
+const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('UserModule (e2e)', () => {
-  let app;
+  let app: INestApplication;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
-
     app = module.createNestApplication();
     await app.init();
   });
 
+  afterAll(async () => {
+    await getConnection().dropDatabase();
+    app.close();
+  });
+
+  describe('createAccount', () => {
+    const EMAIL = 'nico@las.com';
+    it('should create account', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            createAccount(input: {
+              email:"${EMAIL}",
+              password:"12345",
+              role:Owner
+            }) {
+              ok
+              error
+            }
+          }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+
+    it.todo('should fail if account already exists');
+  });
+
   it.todo('me');
   it.todo('userProfile');
-  it.todo('createAccount');
   it.todo('login');
   it.todo('editProfile');
   it.todo('verifyEmail');
