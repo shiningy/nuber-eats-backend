d6507e2dbd020c4097b98cc57f45ffa64237a9ae
myunghyun_yang <shiningy84@gmail.com>
2021-03-13 22:31:32 +0900

    10.2 createRestaurant part 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,6 +18,7 @@ import { Verification } from './users/entities/verification.entity';
 import { MailModule } from './mail/mail.module';
 import { Restaurant } from './restaurants/entities/restaurant.entity';
 import { Category } from './restaurants/entities/category.entity';
+import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
@@ -63,6 +64,7 @@ import { Category } from './restaurants/entities/category.entity';
       fromEmail: process.env.MAILGUN_FROM_EMAIL,
     }),
     UsersModule,
+    RestaurantsModule,
     CommonModule,
     AuthModule,
     MailModule,
