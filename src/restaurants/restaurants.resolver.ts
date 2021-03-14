23030aad11fc35be9249b695ee227939c8fa4710
myunghyun_yang <shiningy84@gmail.com>
2021-03-14 15:22:01 +0900

    11.3 Edit and Delete Dish

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -125,7 +125,7 @@ export class DishResolver {
   createDish(
     @AuthUser() owner: User,
     @Args('input') createDishInput: CreateDishInput,
-  ) {
+  ): Promise<CreateDishOutput> {
     return this.restaurantService.createDish(owner, createDishInput);
   }
 }
