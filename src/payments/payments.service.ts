667d91c6df32612476c810ec49122e489bf76c00
myunghyun_yang <shiningy84@gmail.com>
2021-03-20 01:27:17 +0900

    13.4 getPayments Resolver

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -7,6 +7,7 @@ import {
   CreatePaymentInput,
   CreatePaymentOutput,
 } from './dtos/create-payment.dto';
+import { GetPaymentsOutput } from './dtos/get-payments.dto';
 import { Payment } from './entities/payment.entity';
 
 @Injectable()
@@ -43,6 +44,21 @@ export class PaymentService {
       return { ok: true };
     } catch {
       return { ok: false, error: 'Could not create payment.' };
+    }
+  }
+
+  async getPayments(user: User): Promise<GetPaymentsOutput> {
+    try {
+      const payments = await this.payments.find({ user: user });
+      return {
+        ok: true,
+        payments,
+      };
+    } catch {
+      return {
+        ok: false,
+        error: 'Could not load payments.',
+      };
     }
   }
 }
